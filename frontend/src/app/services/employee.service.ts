331685045d418d434771b0263c2e38363772b6a5
Nadabastaya <garcia.camilo@outlook.com>
2020-05-22 16:28:08 -0400

    add employee function

--- frontend/src/app/services/employee.service.ts
+++ frontend/src/app/services/employee.service.ts
@@ -6,8 +6,25 @@ import { Employee } from '../models/employee';
   providedIn: 'root'
 })
 export class EmployeeService {
+  readonly URL_API = "http://localhost:3000/api/employees";
 
   selectedEmplotee: Employee;
 
   constructor(private http: HttpClient) { }
+
+  getEmployee() {
+    return this.http.get(this.URL_API);
+  }
+
+  postEmployee()  {
+    return this.http.post(this.URL_API, Employee);
+  }
+
+  putEmployee(employee: Employee) {
+    return this.http.put(this.URL_API + `/${employee._id}`, employee);
+  }
+
+  deleteEmployee(_id: string)  {
+    return this.http.delete(this.URL_API + `/${_id}`)
+  }
 }
